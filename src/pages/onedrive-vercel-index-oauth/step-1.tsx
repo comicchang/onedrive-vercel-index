2286d5c0969dc6bb97ec7dd9cdb0b143fbf27e09
Comic Chang <comicchang@gmail.com>
2025-12-29 20:48:15 +0800

    fix compile errors
    Signed-off-by: Comic Chang <comicchang@gmail.com>

--- src/pages/onedrive-vercel-index-oauth/step-1.tsx
+++ src/pages/onedrive-vercel-index-oauth/step-1.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useTranslation, Trans } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import nextI18NextConfig from '../../../next-i18next.config'
 
 import siteConfig from '../../../config/site.config'
 import apiConfig from '../../../config/api.config'
@@ -150,7 +151,7 @@ export default function OAuthStep1() {
 export async function getServerSideProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ['common'], nextI18NextConfig)),
     },
   }
 }
